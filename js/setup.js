06190654075dd7841d60b5b0e258f3c91d7ae7bc
Aleksey <hl-hanter1997@yandex.ru>
2019-06-21 11:46:41 +0500

    Модуль 4, задание 1

--- js/setup.js
+++ js/setup.js
@@ -2,12 +2,15 @@
 
 // Константы
 var QUANTITY_OF_PERSONS = 4;
+var ESC_KEY = 27;
+var ENTER_KEY = 13;
 
 // Массивы данных
 var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SECOND_NAMES = ['да Мария', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // Функция для получения случайного значения
 function getIndex(length) {
@@ -51,12 +54,74 @@ function getDomElemets() {
   return fragment;
 }
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var Settings = document.querySelector('.setup');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarListElement = Settings.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 similarListElement.appendChild(getDomElemets());
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+Settings.querySelector('.setup-similar').classList.remove('hidden');
+
+
+// Модуль 4, задание 1
+var openSettings = document.querySelector('.setup-open');
+var closeSettings = Settings.querySelector('.setup-close');
+var userIcon = document.querySelector('.setup-open-icon');
+userIcon.tabIndex = '0';
+closeSettings.tabIndex = '0';
+
+var coatColor = document.querySelector('.setup-wizard .wizard-coat');
+var eyesColor = document.querySelector('.setup-wizard .wizard-eyes');
+var fireballColor = document.querySelector('.setup-fireball-wrap');
+var saveButton = document.querySelector('.setup-submit');
+
+// Блок для открытия/закрытия поп-апа
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEY && !this.querySelector('.setup-user-name:focus')) {
+    closePopup();
+  }
+}
+
+function openPopup(){
+  Settings.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+
+  coatColor.addEventListener('click', function () {
+    coatColor.style.fill = COAT_COLORS[getIndex(COAT_COLORS.length)];
+  })
+
+  eyesColor.addEventListener('click', function () {
+    eyesColor.style.fill = EYES_COLORS[getIndex(EYES_COLORS.length)];
+  })
+
+  fireballColor.addEventListener('click', function () {
+    fireballColor.style.background = FIREBALL_COLORS[getIndex(FIREBALL_COLORS.length)];
+    fireballColor.querySelector('input').value = FIREBALL_COLORS[getIndex(FIREBALL_COLORS.length)];
+  })
+};
+
+function closePopup() {
+  Settings.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+openSettings.addEventListener('click', function () {
+  openPopup();
+});
+
+userIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+closeSettings.addEventListener('click', function () {
+  closePopup()
+});
+
+closeSettings.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY) {
+    closePopup()
+  }
+});
